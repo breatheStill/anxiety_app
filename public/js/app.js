c785666149e29b489f565e72e8a31f3c20230b07
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-01-12 11:34:22 -0800

    layout form

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,20 @@
 'use strict';
 
 $(() => {
-  $('#new').hide();
+
+  function eventHelper(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  // hide profile, show form
+  $('#profile').hide();
+  // $('#new').hide();
+  $('#profile button').on('click', e => {
+    eventHelper(e);
+
+    $('#profile').hide();
+    $('#new').show();
+  });
+
 });
