a5570f59299854427f0e60140f509fd290122a59
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-01-16 10:05:54 -0800

    testing something in charts

--- public/js/app.js
+++ public/js/app.js
@@ -11,21 +11,43 @@ $(() => {
     $('#new').toggle();
   }
 
-  // hide 
-  $('#profile').hide();
-  $('#new').hide();
-  $('#main-graph').show();
+
+  // get oist of jQuery objedts
+  const components = [
+    '#mainGraph',
+    '#profile',
+    '#new',
+  ];
+
+  function showPage (page) {
+    components.forEach(element => {
+      if (element === page) $(`${element}`).show();
+      else $(`${element}`).hide();
+    });
+  }
 
   // hide profile, show form
   $('#profile button').on('click', e => {
     eventHelper(e);
-    toggleProfileForm();
+    // toggleProfileForm();
+    showPage('#new');
   });
 
   // hide form, show profile
   $('a.cancel').on('click', e => {
     eventHelper(e);
-    toggleProfileForm();
+    // toggleProfileForm();
+    showPage('#profile');
   });
 
-});
+  $('#smallGraph').on('click', e => {
+    eventHelper(e);
+    // $('#profile').hide();
+    // $('canvas#myMainChart').show();
+    // $('#mainGraph').show();
+    showPage('#mainGraph');
+  });
+
+   showPage('#profile');
+
+  });
