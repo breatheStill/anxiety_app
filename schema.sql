a7f22c1ee4e7f06c2994a2dd3dc4f6376bcdd5b1
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-01-22 19:25:35 -0800

    fix schema, add timestamp to suggestions

--- schema.sql
+++ schema.sql
@@ -10,8 +10,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TABLE IF EXISTS journals;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS suggestions;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id uuid DEFAULT uuid_generate_v4 (),
@@ -24,6 +24,7 @@ CREATE TABLE journals (
   id SERIAL PRIMARY KEY,
   uid uuid NOT NULL REFERENCES users(id),
   date DATE,
+  entered TIMESTAMP DEFAULT now(),
   exercise BOOLEAN,
   outdoors BOOLEAN,
   entry TEXT,
@@ -36,7 +37,12 @@ CREATE TABLE journals (
 );
 
 CREATE TABLE suggestions (
+  id SERIAL PRIMARY KEY,
   suggestion TEXT,
   name TEXT,
-  id SERIAL PRIMARY KEY
+  entered TIMESTAMP DEFAULT now()
 );
+
+
+
+-- Copy Journal Entries
